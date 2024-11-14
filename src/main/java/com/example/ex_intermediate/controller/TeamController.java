8efb9f3e9ac8ea3f5d93bdcd6211b04c5709135e
Hara Yuya <harayuya.liamg@gmail.com>
2024-11-14 11:15:02 +0900

    TeamControllerのshowList()の実装

---
+++ src/main/java/com/example/ex_intermediate/controller/TeamController.java
@@ -0,0 +1,20 @@
+package com.example.ex_intermediate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import com.example.ex_intermediate.service.TeamService;
+
+@Controller
+public class TeamController {
+    
+    @Autowired
+    private TeamService service;
+
+    @RequestMapping("/show-list")
+    public String showList(Model model) {
+        model.addAttribute("teamList", service.findAll());
+        return "show-list";
+    }
+}

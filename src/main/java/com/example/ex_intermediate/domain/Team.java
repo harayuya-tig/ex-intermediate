d74cb1a2ffb0c9d6d94a45cb32ae1e0c7560a723
Hara Yuya <harayuya.liamg@gmail.com>
2024-11-14 10:26:20 +0900

    Teamドメインの微修正 漏れていたinaugurationフィールドの追加

--- src/main/java/com/example/ex_intermediate/domain/Team.java
+++ src/main/java/com/example/ex_intermediate/domain/Team.java
@@ -1,11 +1,14 @@
 package com.example.ex_intermediate.domain;
 
+import java.time.LocalDate;
+
 public class Team {
     
     private Integer id;
     private String leagueName;
     private String teamName;
     private String headQuaters;
+    private LocalDate inauguration;
     private String history;
     
     public Integer getId() {
@@ -31,6 +34,12 @@ public class Team {
     }
     public void setHeadQuaters(String headQuaters) {
         this.headQuaters = headQuaters;
+    }
+    public LocalDate getInauguration() {
+        return inauguration;
+    }
+    public void setInauguration(LocalDate inauguration) {
+        this.inauguration = inauguration;
     }
     public String getHistory() {
         return history;

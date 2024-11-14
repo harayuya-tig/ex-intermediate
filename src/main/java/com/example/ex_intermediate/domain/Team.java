09813b3203beb308451c12875a9406d10bf0817a
Hara Yuya <harayuya.liamg@gmail.com>
2024-11-14 10:10:48 +0900

    Teamドメインの作成

---
+++ src/main/java/com/example/ex_intermediate/domain/Team.java
@@ -0,0 +1,46 @@
+package com.example.ex_intermediate.domain;
+
+public class Team {
+    
+    private Integer id;
+    private String leagueName;
+    private String teamName;
+    private String headQuaters;
+    private String history;
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getLeagueName() {
+        return leagueName;
+    }
+    public void setLeagueName(String leagueName) {
+        this.leagueName = leagueName;
+    }
+    public String getTeamName() {
+        return teamName;
+    }
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+    public String getHeadQuaters() {
+        return headQuaters;
+    }
+    public void setHeadQuaters(String headQuaters) {
+        this.headQuaters = headQuaters;
+    }
+    public String getHistory() {
+        return history;
+    }
+    public void setHistory(String history) {
+        this.history = history;
+    }
+    @Override
+    public String toString() {
+        return "Team [id=" + id + ", leagueName=" + leagueName + ", teamName=" + teamName + ", headQuaters="
+                + headQuaters + ", history=" + history + "]";
+    }
+}
